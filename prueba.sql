2b3b7aa6917e096871b345f6c3e8b68e0afe5620
bryanets <bryantrebejo@hotmail.com>
2023-06-19 05:23:13 -0500

    base de datos
    actualizacion de la relacion matricula - cursos

--- prueba.sql
+++ prueba.sql
@@ -82,11 +82,14 @@ CREATE TABLE `cursos` (
   `creditos` int DEFAULT NULL,
   `id_carrera` int DEFAULT NULL,
   `id_profesor` int DEFAULT NULL,
+  `id_matricula` int DEFAULT NULL,
   PRIMARY KEY (`id_curso`),
   KEY `cursos_ibfk_1` (`id_carrera`),
   KEY `cursos_ibfk_2` (`id_profesor`),
+  KEY `cursos_ibkf_3_idx` (`id_matricula`),
   CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id_carrera`) ON DELETE CASCADE,
-  CONSTRAINT `cursos_ibfk_2` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`) ON DELETE CASCADE
+  CONSTRAINT `cursos_ibfk_2` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`) ON DELETE CASCADE,
+  CONSTRAINT `cursos_ibkf_3` FOREIGN KEY (`id_matricula`) REFERENCES `matricula` (`id_matricula`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -96,7 +99,7 @@ CREATE TABLE `cursos` (
 
 LOCK TABLES `cursos` WRITE;
 /*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
-INSERT INTO `cursos` VALUES (1,'Introducción a la algoritmia',3,1,1),(2,'Arquitectura de Entornos Web',3,1,1),(3,'Tecnologías de la Información',2,1,1),(4,'Fundamentos de Gestión Empresarial',2,1,1),(5,'Matemática I',2,1,1),(6,'Desarrollo de Habilidades Profesionales I',2,1,1);
+INSERT INTO `cursos` VALUES (1,'Introducción a la algoritmia',3,1,1,NULL),(2,'Arquitectura de Entornos Web',3,1,1,NULL),(3,'Tecnologías de la Información',2,1,1,NULL),(4,'Fundamentos de Gestión Empresarial',2,1,1,NULL),(5,'Matemática I',2,1,1,NULL),(6,'Desarrollo de Habilidades Profesionales I',2,1,1,NULL);
 /*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +147,7 @@ CREATE TABLE `horarios` (
   PRIMARY KEY (`id_horario`),
   KEY `id_curso` (`id_curso`),
   CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,6 +156,7 @@ CREATE TABLE `horarios` (
 
 LOCK TABLES `horarios` WRITE;
 /*!40000 ALTER TABLE `horarios` DISABLE KEYS */;
+INSERT INTO `horarios` VALUES (2,2,'Lunes','08:00:00','09:30:00','T1DN'),(4,1,'Martes','09:45:00','11:15:00','T1DN'),(5,1,'Miercoles','02:30:00','04:15:00','T1TN'),(6,2,'Jueves','02:30:00','04:15:00','T1TN');
 /*!40000 ALTER TABLE `horarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,15 +171,12 @@ CREATE TABLE `matricula` (
   `id_matricula` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int DEFAULT NULL,
   `id_carrera` int DEFAULT NULL,
-  `id_curso` int DEFAULT NULL,
   `fecha_matricula` date DEFAULT NULL,
   PRIMARY KEY (`id_matricula`),
   KEY `id_usuario` (`id_usuario`),
   KEY `id_carrera` (`id_carrera`),
-  KEY `id_curso` (`id_curso`),
   CONSTRAINT `matricula_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
-  CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id_carrera`),
-  CONSTRAINT `matricula_ibfk_3` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`)
+  CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id_carrera`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -267,7 +268,7 @@ CREATE TABLE `tb_enlace` (
 
 LOCK TABLES `tb_enlace` WRITE;
 /*!40000 ALTER TABLE `tb_enlace` DISABLE KEYS */;
-INSERT INTO `tb_enlace` VALUES (1,'login','/usuasrio/validar'),(2,'usuario','/usuario/lista'),(3,'carreras','/carrera/lista'),(4,'cursos','/curso/lista'),(5,'matriculas','/matricula/lista'),(6,'horarios','/horario/lista'),(7,'intranet','/usuario/intranet');
+INSERT INTO `tb_enlace` VALUES (2,'usuarios','/usuario/lista'),(3,'carreras','/carrera/lista'),(4,'cursos','/curso/lista'),(5,'matriculas','/matricula/lista'),(6,'horarios','/horario/lista'),(7,'intranet','/usuario/intranet');
 /*!40000 ALTER TABLE `tb_enlace` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -318,7 +319,7 @@ CREATE TABLE `tb_rol_enlace` (
 
 LOCK TABLES `tb_rol_enlace` WRITE;
 /*!40000 ALTER TABLE `tb_rol_enlace` DISABLE KEYS */;
-INSERT INTO `tb_rol_enlace` VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7);
+INSERT INTO `tb_rol_enlace` VALUES (1,2),(1,3),(1,4),(1,5),(1,6),(1,7);
 /*!40000 ALTER TABLE `tb_rol_enlace` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -344,7 +345,7 @@ CREATE TABLE `usuario` (
   KEY `fk_usuario_rol_idx` (`idrol`),
   KEY `llave_idx` (`idrol`),
   CONSTRAINT `ghgh` FOREIGN KEY (`idrol`) REFERENCES `tb_rol` (`idrol`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -353,7 +354,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'bryan','trebejo','bryantrebejo@hotmail.com','$2a$10$cKE.Y1BJHk.6Skw53xTbDO8MGdE2woJgQkZJ9GUuDlcynFTIcmh8G',1,'bryan','47586985'),(14,'frank','mendoza','i201821212@cibertec.edu.pe','wqwwqwqwq',3,'frank20','12345678'),(15,'Armando','Scripts','sarmando@cibertec.pe',NULL,4,'armando','85203698');
+INSERT INTO `usuario` VALUES (1,'bryan','trebejo','bryantrebejo@hotmail.com','$2a$10$cKE.Y1BJHk.6Skw53xTbDO8MGdE2woJgQkZJ9GUuDlcynFTIcmh8G',1,'bryan','47586985'),(14,'frank','mendoza','i201821212@cibertec.edu.pe','wqwwqwqwq',3,'frank20','12345678'),(15,'Armando','Scripts','sarmando@cibertec.pe',NULL,4,'armando','85203698'),(16,'benedicto','XIV','papita@hotmail.com','$2a$10$hQUVDh6uWf/ioBfKagMbu.mCNfCdgLsYFPm3SOEZUx7bOSvrYnG/C',3,'papaxiv','58963247');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -364,6 +365,29 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'prueba'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `cursos_carrera` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cursos_carrera`(Vcod int)
+BEGIN
+select ca.id_carrera,ca.nombre_carrera, cu.nombre_curso,cu.creditos, ci.descripcion,cc.id_ciclo from carrera ca 
+join cursos cu on cu.id_carrera=ca.id_carrera 
+join cursos_ciclo cc on cc.id_curso=cu.id_curso
+join ciclo_academico ci on ci.id_ciclo=cc.id_ciclo 
+where ca.id_carrera=Vcod; 
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -374,4 +398,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-16  7:07:17
+-- Dump completed on 2023-06-19  5:22:10
